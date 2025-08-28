db8f4cf89bbd725c8eba16afeb4c35627b30f75b
Altin Demku <altin-demko@hotmail.de>
2025-08-28 20:42:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '<PASSWORD>'
